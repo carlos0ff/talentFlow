e281d724f5804cfa3c2637c9649ae0ad55628082
José Carlos da Silva Júnior <carlosiilva66@gmail.com>
2026-03-02 16:09:49 -0300

    feat: atualiza rotas públicas

--- routes/public.php
+++ routes/public.php
@@ -1,10 +1,13 @@
 <?php
 declare(strict_types=1);
 
-
 use Illuminate\Support\Facades\Route;
 
+/** Controllers  */
 
+/**
+ *
+ */
 Route::get('/', function () {
      return Inertia::render('Welcome');
 })->name('home');
