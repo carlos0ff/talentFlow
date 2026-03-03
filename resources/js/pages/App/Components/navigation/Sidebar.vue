c9e1ff061e620d2f2672cc5d8ba6e3cea480c530
José Carlos da Silva Júnior <carlosiilva66@gmail.com>
2026-03-03 12:02:38 -0300

    feat: adiciona Sidebar

---
+++ resources/js/pages/App/Components/navigation/Sidebar.vue
@@ -0,0 +1,15 @@
+<template>
+
+    <!-- Side -->
+    <div id="sidebar" class="fixed left-0 top-0 h-full w-64 bg-slate-900 text-white z-50 sidebar-transition transform -translate-x-full lg:translate-x-0">
+        <!-- Logo -->
+        <div class="p-6 border-b border-slate-700 text-center">
+            <h1 class="text-xl font-bold">
+                <span class="text-blue-400 mb-2">Fada Group</span>
+                <span class="text-sm text-gray-300 font-normal">
+                    Quality for Passion
+                </span>
+            </h1>
+        </div>
+    </div>
+</template>
